caf362f2973e5d9e70121c4e3ce19d54a0c47ffb
Helena Reid <helena.reid@metoffice.gov.uk>
2025-10-02 14:34:44 +0100

    start project

---
+++ src/isca_analysis/socrates_test.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+import os
+def main():
+    print("running analysis")
+    isca_datadir = Path(os.getenv("GFDL_DATA"))
+    print(f"{isca_datadir=}")
+    exp_datadir = isca_datadir.joinpath("soc_aquaplanet", "run_0001")
+
+
+if __name__=="__main__":
+    main()
